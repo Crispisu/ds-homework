6e8b788117851a40633468ca9ec5c23968c3b817
Cristina Dragomir <crisd@Cristinas-MacBook-Air.local>
2021-10-06 13:00:57 +0300

    Si ex 2

--- session3/ex2.py
+++ session3/ex2.py
@@ -7,8 +7,15 @@
 # si variabilei l2 ca valoare o lista cu 4 elemente (5,6,7)
 l1 = [1, 2, 3, 4]
 l2 = [5, 6, 7]
+l3 = l1 + l2
 
 # Afisam listele l1 si l2 separat.
 # Pentru a vedea rezultatul, rulati acest script.
 print(l1)
 print(l2)
+print(l3)
+# sau, mai simplu, direct:
+print (l1+l2)
+
+l1.extend(l2)
+print(l1)
