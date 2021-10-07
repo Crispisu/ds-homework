cf31e6ee15e26a824a958c78329e8315dc4922b9
Cristina Dragomir <crisd@Cristinas-MacBook-Air.local>
2021-10-07 15:16:32 +0300

    Gata 15

--- session3/ex15.py
+++ session3/ex15.py
@@ -6,3 +6,12 @@
         Veti primi: 'cmi'
         Veti printa: 1
 """
+
+x = input()
+vocale = "aeiou"
+count = 0
+for i in range (0,len(x)):
+    if x[i] in vocale:
+        count = count + 1
+    
+print (count)
