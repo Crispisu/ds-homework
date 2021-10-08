a2873bb033ecfe0a3e3d206259bd27f3cd68ba65
Cristina Dragomir <crisd@Cristinas-MacBook-Air.local>
2021-10-08 22:42:26 +0300

    Gata 22

--- session3/ex22.py
+++ session3/ex22.py
@@ -6,3 +6,13 @@
         Veti primi: 'center'
         Veti printa: 'CeNtEr'
 """
+x = input()
+y = ""
+
+for i in range (0,len(x)):
+    if i % 2 == 0:
+        y += x[i].upper()
+    else:
+        y += x[i].lower()
+
+print(y)
