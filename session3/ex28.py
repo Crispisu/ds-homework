b92324dd0306b53d254c6a464f7015a1fdcf8eb9
Cristina Dragomir <crisd@Cristinas-MacBook-Air.local>
2021-10-10 10:08:52 +0300

    Gata si 28

--- session3/ex28.py
+++ session3/ex28.py
@@ -6,3 +6,21 @@
         Veti primi: 5
         Veti printa: 15
 """
+x = int(input())
+s = 0
+l = []
+
+for i in range (1,x+1):
+    l.append(i)
+    s = sum(l)
+    
+print (s)
+
+#sau:
+
+x = int(input())
+s = 0
+for i in range (1,x+1):
+    s = s + i
+    
+print (s)
