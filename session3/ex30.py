d3a80cb30ff6f486c81cb50a0d2799e3ed0aa70e
Cristina Dragomir <crisd@Cristinas-MacBook-Air.local>
2021-10-11 20:18:07 +0300

    30 modificat

--- session3/ex30.py
+++ session3/ex30.py
@@ -12,49 +12,58 @@
 """
 x = input()
 l = []
+pop_previous = False
 
 for i in x:
     l.append(i)
 try:
     for element in l:
+        
         if element == "(":
-            l.pop(l.index(")"))
+            l.pop(l.index(")",l.index(element)))
+            
         if element == "[":
-            l.pop(l.index("]"))
+            l.pop(l.index("]",l.index(element)))
+            
         if element == "{":
-            l.pop(l.index("}"))
+            l.pop(l.index("}",l.index(element)))
+            
+            
 except ValueError:
     print (False)
 else:
-    print (True)
+    if l.count(")") > 0 or l.count("]") > 0 or l.count("}") > 0:
+        print(False)
+    else:
+        print (True)
 
-# else:
+# else: (asta nu o mai folosim)
 
-x = input()
-r_d = 0
-r_i = 0
-p_d = 0
-p_i = 0
-a_d = 0
-a_i = 0
+# x = input()
+# r_d = 0
+# r_i = 0
+# p_d = 0
+# p_i = 0
+# a_d = 0
+# a_i = 0
 
-for i in x:
-    if i == "(":
-        r_d += 1
-    if i == ")":
-        r_i += 1
-    if i == "[":
-        p_d += 1
-    if i == "]":
-        p_i += 1
-    if i == "{":
-        a_d += 1
-    if i == "}":
-        a_i += 1
-if r_d == r_i and p_d == p_i and a_d == a_i:
-    print(True)
-else: 
-    print(False)
+# for i in x:
+#     if i == "(":
+#         r_d += 1
+#     if i == ")":
+#         r_i += 1
+#     if i == "[":
+#         p_d += 1
+#     if i == "]":
+#         p_i += 1
+#     if i == "{":
+#         a_d += 1
+#     if i == "}":
+#         a_i += 1
+# if r_d == r_i and p_d == p_i and a_d == a_i:
+#     print(True)
+# else: 
+#     print(False)
     
 
 
