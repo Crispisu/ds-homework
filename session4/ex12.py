a1cc735f48d7880d1415f7eef64295a406d55852
Cristina Dragomir <crisd@Cristinas-MacBook-Air.local>
2021-10-16 13:59:17 +0300

    12 done

--- session4/ex12.py
+++ session4/ex12.py
@@ -5,8 +5,24 @@
     Observatii: f nu e la fel ca la ex 11.
 
 """
+#file = open("output12.data","x")
 
-
+def dec(func):
+    def wrapper (valoare):
+        f = open("output12.data", "w")
+        f.write(valoare)
+        f.close()
+    return wrapper
+    
 # decorate me
+@dec
 def f(x):
     print(x)
+
+f("jucarie")
+
+file = open("output12.data","r")
+print(file.read())
+file.close()
+
+
