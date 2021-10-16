dfd74592b1a2d31ffb9549d59ff99a204f4ac385
Cristina Dragomir <crisd@Cristinas-MacBook-Air.local>
2021-10-16 12:24:15 +0300

    In sfarsit si 11

--- session4/ex11.py
+++ session4/ex11.py
@@ -6,8 +6,19 @@
 
     Functia decorata este f.
 """
-
+def dec(func):
+    f = open("output11.data", "w")
+    f.write(func())
+      
 
 # decorate me
+@dec
 def f():
     return "CMI"
+
+f
+
+file = open("output11.data","r")
+print(file.read())
+
+
