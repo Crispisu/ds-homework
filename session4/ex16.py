36b6f4b87ffc0464647ebf78f8db999e731c7d3f
Cristina Dragomir <crisd@Cristinas-MacBook-Air.local>
2021-10-18 23:01:41 +0300

    16 done

--- session4/ex16.py
+++ session4/ex16.py
@@ -19,3 +19,45 @@
         - veti primi input: 'cEVa1'
             ---> ceva1
 """
+
+
+
+
+# def call_changers(func):
+#     x = input()
+#     my_str = str(x)
+#     if len(my_str) % 2 == 0:
+#         def up_str(my_str):
+#             return my_str.upper()
+#     else:
+#         def low_str(my_str):
+#             return my_str.lower()
+#     print(call_changers(x))
+
+# x = input()
+# my_str = str(x)
+   
+# def up_str(my_str):
+#     return my_str.upper()
+    
+# def low_str(my_str):
+#     return my_str.lower()
+
+# def call_changers(func,x):
+#     print(up_str(x)) if len(my_str) % 2 == 0 else print(low_str(x))
+
+# call_changers(up_str(x),low_str(x))
+
+x = input()
+my_str = str(x)
+   
+def up_str(my_str):
+    return my_str.upper()
+    
+def low_str(my_str):
+    return my_str.lower()
+
+def call_changers(func,my_str):
+    print(func(my_str))
+
+call_changers(up_str,my_str) if len(my_str) % 2 == 0 else call_changers(low_str,my_str)
