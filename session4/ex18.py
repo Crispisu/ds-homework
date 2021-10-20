1b03b2297398f95c1a174884cd846bc5de03da64
Cristina Dragomir <crisd@Cristinas-MacBook-Air.local>
2021-10-20 23:35:51 +0300

    18 done

--- session4/ex18.py
+++ session4/ex18.py
@@ -6,3 +6,13 @@
         - f([1,2,3])
             ---> 6
 """
+
+def func(lista):
+    lista = list(lista)
+    if len(lista) == 1:
+        return lista[0]
+    element = lista[0]
+    lista.pop(0)
+    return element + func(lista)
+
+print (func([1,2,3,4]))
