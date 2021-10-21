a171e5288f1eadecff8aefdd6aceb52b00358fcb
Cristina Dragomir <crisd@Cristinas-MacBook-Air.local>
2021-10-21 23:47:15 +0300

    19 done

--- session4/ex19.py
+++ session4/ex19.py
@@ -16,3 +16,47 @@
             }
 
 """
+
+import random
+import string 
+import json
+
+
+# def func(x):
+#     x = str(x)
+#     try:
+#         file = open(x + '.json',"w")
+#     except OSError:
+#         file = open(x + '.json',"x")
+#     finally:
+#         file.write('{ \n')
+#     for i in range (0,4):
+#         key = random.randint(0,10)
+#         alfabet = string.ascii_lowercase
+#         string_len = random.randint(3,6)
+#         value = ''.join(random.choice(alfabet) for i in range (string_len))
+#         file.write('\t' + str(key) + ': \'' + value + '\' \n')
+#     file.write('}')
+#     file.close()
+
+# func('pisimic')
+
+def func(x):
+    x = str(x)
+    dictionar = {}
+    try:
+        file = open(x + '.json',"w")
+    except OSError:
+        file = open(x + '.json',"x")
+    for i in range (0,4):
+        key = random.randint(0,10)
+        alfabet = string.ascii_lowercase
+        string_len = random.randint(3,6)
+        value = ''.join(random.choice(alfabet) for i in range (string_len))
+        dictionar[key] = value
+    dict = json.dumps(dictionar)
+    file.write(dict)
+    file.close()
+    
+
+func('pisimic2')
