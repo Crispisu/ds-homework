f0c82925bdcf79c15fc0887e007ed7309f315179
Cristina Dragomir <crisd@Cristinas-MacBook-Air.local>
2021-10-23 22:29:57 +0300

    20 done

--- session4/ex20.py
+++ session4/ex20.py
@@ -5,3 +5,19 @@
     Toate astea le veti face intr-o functie read_from_file(file), unde
     file este numele fisierului primit dat ca parametru.
 """
+import json
+
+# def read_from_file(file):
+#     with open ('pisimic2.json') as file:
+#         content = file.read()
+#         print(content)
+
+# read_from_file('pisimic2.json')
+
+def read_from_file(filename):
+    with open (filename) as file:
+        content = file.read()
+        dictionar = json.loads(content)
+        print(dictionar)
+
+read_from_file('pisimic2.json')
